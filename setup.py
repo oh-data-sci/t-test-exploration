b931b65032e53a7188cb89ca76f8331ae379ef6b
oskar holm <oholm@brandwatch.com>
2019-11-18 11:17:22 +0000

    20191118 added setup and local project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='t-test-exploration',
+    packages=find_packages(),
+    version='0.1.0',
+    description='exploration into suitability of a data set for t-test.',
+    author='oh-data-sci',
+)
